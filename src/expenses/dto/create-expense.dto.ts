94c0e0839991f52d99d6dd561833a9b47a39daf5
Daniel Lopez <dalobe56@gmail.com>
2025-01-21 18:04:29 -0500

    Add some code to expenses

--- src/expenses/dto/create-expense.dto.ts
+++ src/expenses/dto/create-expense.dto.ts
@@ -1,1 +1,16 @@
-export class CreateExpenseDto {}
+import { IsNumber, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreateExpenseDto {
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+
+    @IsNumber()
+    @IsPositive()
+    amount: number;
+    
+    @IsString()
+    category: string;
+
+}
--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateExpenseDto } from './dto/create-expense.dto';
 import { UpdateExpenseDto } from './dto/update-expense.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Expense } from './entities/expense.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ExpensesService {
-  create(createExpenseDto: CreateExpenseDto) {
-    return 'This action adds a new expense';
+
+  constructor(
+    @InjectRepository(Expense)
+    private readonly expenseRepository: Repository<Expense>
+  ) {}
+
+  async create(createExpenseDto: CreateExpenseDto) {
+    return createExpenseDto;
   }
 
   findAll() {
     return `This action returns all expenses`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} expense`;
   }
 
-  update(id: number, updateExpenseDto: UpdateExpenseDto) {
+  update(id: string, updateExpenseDto: UpdateExpenseDto) {
     return `This action updates a #${id} expense`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} expense`;
   }
 }
