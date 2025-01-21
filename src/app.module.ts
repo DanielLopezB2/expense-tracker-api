f0bf9abfd5a0017f8fb0e82f86704f03aacc8354
Daniel Lopez <dalobe56@gmail.com>
2025-01-21 15:28:13 -0500

    Add new pakcages and needed modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ExpensesModule } from './expenses/expenses.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    AuthModule,
+    ExpensesModule,
+    CategoriesModule
+  ],
   controllers: [],
   providers: [],
 })
