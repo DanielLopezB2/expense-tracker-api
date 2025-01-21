8475e5e05a75538fdc7fc3de80dd8d9dbbf5d36a
Daniel Lopez <dalobe56@gmail.com>
2025-01-21 17:49:10 -0500

    Add starters for login and users creation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(
+
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) {}
+
+  async register(createUserDto: CreateUserDto) {
+
+    try {
+      const { password, ...userData } = createUserDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10)
+      });
+  
+      await this.userRepository.save(user);
+
+      const { password: _, updatedAt, deletedAt, ...restUser } = user;
+      return restUser;
+  
+    } catch (error) {
+      this.handleDbExceptions(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
+  async findOne(id: string) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOneByEmail(email: string) {
+
+    const user = await this.userRepository.findOne(
+      {
+        where: { email },
+        select: { email: true, password: true, id: true }
+      }
+    );
+
+    if (!user) throw new NotFoundException(`User with email ${email} not found.`)
+    
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  private handleDbExceptions(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    throw new InternalServerErrorException('Unexpected error, check server logs')
   }
+
 }
